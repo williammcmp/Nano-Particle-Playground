f37b79accc86cb0c402468157f4bd6c6a6a14b5a
William <william.mcm.p@gmail.com>
2023-09-30 10:53:31 +1000

    added GenerateNanoParticles function and renamed particle generation functions

--- Particle.py
+++ Particle.py
@@ -51,11 +51,11 @@ class Particle:
     
 
 # Creates a large amount of particles
-def ParticleCreation(n, Simulation):
+def GenerateParticles(n, Simulation):
     """
     Create a specified number of particles and add them to the simulation.
 
-    This function generates random positions, velocities, masses, and charges for a given number of particles
+    This function generates random positions (m), velocities (m/s), masses (kg), and charges (-1,0,+1) for a given number of particles
     and then creates Particle objects with these properties. These particles are added to the provided Simulation object.
 
     Parameters:
@@ -74,7 +74,7 @@ def ParticleCreation(n, Simulation):
     Returns:
     None
     """
-    print("\nGenerating particles:")
+    print(f"\nGenerating {n} Particles:")
     for x in tqdm(range(n), unit=" Particle(s)"):
         position = np.random.uniform(0, 3, 3) + np.array([0,0,1])
         velocity = np.random.uniform(-2, 2, 3)
@@ -83,7 +83,7 @@ def ParticleCreation(n, Simulation):
         Simulation.Particles.append(Particle(position, velocity, mass, charge))
 
 
-def LoadTestParticles(sim):
+def GenerateTestParticles(Simulation):
     """
     Load a set of predefined test particles into a simulation.
 
@@ -105,11 +105,40 @@ def LoadTestParticles(sim):
     Returns:
     None
     """
+    print("\nLoading Test Particles:")
     # Create a particle and add it to the simulation
     p1 = Particle([0, 0, 0], [0, 1, 3], 1, -1)
     p2 = Particle([0, 0, 0], [0, 1, 3], 1, 1)
     p3 = Particle([0, 0, 0], [0, 1, 3], 1, 0)
-    sim.Particles.append(p1)
-    sim.Particles.append(p2)
-    sim.Particles.append(p3)
+    Simulation.Particles.append(p1)
+    Simulation.Particles.append(p2)
+    Simulation.Particles.append(p3)
+
+def GenerateNanoParticles(n, Simulation):
+    """
+    Generates and adds Nano-Particles (modeling SiNPs) to a given simulation.
+
+    This function generates a specified number of Nano-Particles and adds them to a provided Simulation instance.
+
+    Parameters:
+    - n (int): The number of Nano-Particles to generate.
+    - Simulation (Simulation): The Simulation instance to which the particles will be added.
+
+    Returns:
+    None
+
+    Example:
+    ```
+    sim = Simulation()
+    GenerateNanoParticles(100, sim)
+    ```
+    """
+    print(f"\nGenerating {n} Nano-Particles:")
+    for x in tqdm(range(n), unit=" Particle(s)"):
+        # between 20 mu meters
+        position = np.array([0.0000001,0,0]) # all start from origin
+        velocity = np.array([np.random.normal(loc=0, scale=10),np.random.normal(loc=0, scale=10),random.randint(0,10)]) * 1e-7 
+        mass = np.abs(np.random.normal(loc=100, scale=25))* 1e-19  # mass of particles (~ 10^-19kg)
+        charge = 0;
+        Simulation.Particles.append(Particle(position, velocity, mass, charge))
 
