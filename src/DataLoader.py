48cca684ea504cb0253994ef745a8fcfcba8ecb4
William McMahon-Puce <william.mcm.p@gmail.com>
2023-10-14 13:41:04 +1100

    Load top and bottom values

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -80,11 +80,49 @@ def load_experimental_data(experiment_type):
             
             # Increasing the distance from the creator for further images
             max_value = data["X"].max() # max value of the current image
-            min_value = data["X"].min() # min value of the whole dataset
+            min_value = data_df["X"].min() # min value of the whole dataset
 
 
             # Increasing by the pervious image's max positional value
-            data['X'] = min_value - (data['X'] + max_value)
+            data['X'] = min_value - (max_value - data['X'])
+
+
+            # Append the data to the combined data frame
+            data_df = pd.concat([data_df, data], ignore_index=True)
+
+        # Positive Y-axis
+        if experimental_csv[0] == "R": # Above the creator
+            
+            # Create a new DataFrame for the data
+            data = pd.DataFrame(data=experimental_data, columns=["X", "Y", "Width"])
+            
+            # Modify the 'Y' values
+            data["X"] *= 0
+            
+            # Increasing the distance from the creator for further images
+            max_value = data_df["Y"].max() # max value of the whole data set
+
+            # Increasing by the pervious image's max positional value
+            data['Y'] = data['Y'] + max_value
+
+            # Append the data to the combined data frame
+            data_df = pd.concat([data_df, data], ignore_index=True)
+
+        # Negative Y-axis 
+        elif experimental_csv[0] == "L": # Bellow the crator
+            # Create a new DataFrame for the data
+            data = pd.DataFrame(data=experimental_data, columns=["X", "Y", "Width"])
+            
+            # Set Values to zero. 
+            data["X"] *= 0
+            
+            # Increasing the distance from the creator for further images
+            max_value = data["Y"].max() # max value of the current image
+            min_value = data_df["Y"].min() # min value of the whole dataset
+
+
+            # Increasing by the pervious image's max positional value
+            data['Y'] = min_value - (max_value - data['Y'])
 
             # Append the data to the combined data frame
             data_df = pd.concat([data_df, data], ignore_index=True)
@@ -99,5 +137,20 @@ def load_experimental_data(experiment_type):
 
 
 
-test = load_experimental_data("BFieldOut")
-print(test)
+data_df = load_experimental_data("BFieldOut")
+
+
+import matplotlib.pyplot as plt
+
+# Assuming you already have your DataFrame data_df
+
+# Plot the 'X' and 'Y' columns
+plt.scatter(data_df['X'], data_df['Y'])
+
+# Customize the plot (optional)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('X vs. Y')
+
+# Show the plot
+plt.show()
