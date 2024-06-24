fcfbcede77e73974a61613d5ea73ec67fce9295e
William <william.mcm.p@gmail.com>
2024-06-24 21:49:15 +1000

    Implemented numericalunits

--- pages/silicon.py
+++ pages/silicon.py
@@ -4,7 +4,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import NTMpy as ntm
 import random
+import numericalunits as u
+u.reset_units('SI') # makes units easer to read and follow
 
 from src.Particle import Particle
 from src.Simulation import Simulation
@@ -69,77 +72,90 @@ with col2:
         """,
         unsafe_allow_html=True,)
     
-with st.expander("Laser Settins"):
-    with slider_col:
+with st.expander("Laser Settings"):
     options = ['PHAROS', 'SpitFire', 'Custom']
-    laster_option = st.selectbox('Select the laser system.', options, index=0, disabled=True)
+    col1, col2, col3 = st.columns([1,1,1])
+
+    with col1:
+        laser_option = st.selectbox('Select the laser system.', options, index=0, disabled=True)
+
     # Laser input settings
-    if laster_option == 'SpitFire':
-    
-        wavelength = 1064 * 1e-9 # default wavelength of 640 nm
-        laser_power = 0.01 # default power of 1 W
-        pulse_rate = 1 * 1e3 # default pulse rate MHz
-        pulse_duration = 100 * 1e-13 # default pulse duration of 100 fs
-        numerical_aperture = 0.10 # default NA of 0.1
-        variables_dict = {
-            'Wavelength (m)': f'{wavelength:.3g}',
-            'Laser Power (W)': f'{laser_power:.3g}',
-            'Pulse Rate (Hz)': f'{pulse_rate:.3g}',
-            'Pulse Duration (s)': f'{pulse_duration:.3g}',
-            'Numerical Aperture': f'{numerical_aperture:.3g}'
-        }
-        # Convert the dictionary into a DataFrame
-        variables_df = pd.DataFrame(variables_dict, index=['Value']).T
+if laser_option == 'SpitFire':
+    wavelength = 1064 * u.nm  # default wavelength of 1064 nm
+    laser_power = 0.01 * u.W  # default power of 0.01 W
+    pulse_rate = 1 * u.kHz  # default pulse rate of 1 kHz
+    pulse_duration = 100 * u.fs  # default pulse duration of 100 fs
+    numerical_aperture = 0.10  # default NA of 0.1
 
+    variables_dict = {
+        'Wavelength (m)': f'{wavelength:.3g}',
+        'Laser Power (W)': f'{laser_power:.3g}',
+        'Pulse Rate (Hz)': f'{pulse_rate:.3g}',
+        'Pulse Duration (s)': f'{pulse_duration:.3g}',
+        'Numerical Aperture': f'{numerical_aperture:.3g}'
+    }
+
+    # Convert the dictionary into a DataFrame
+    variables_df = pd.DataFrame(variables_dict, index=['Value']).T
+
+
+elif laser_option == "PHAROS":
+    wavelength = 1030 * u.nm  # default wavelength of 1030 nm
+    laser_power = 7 * u.W  # default power of 7 W
+    pulse_rate = 200 * u.kHz  # default pulse rate of 200 kHz
+    pulse_duration = 100 * u.fs  # default pulse duration of 100 fs
+    numerical_aperture = 0.14  # default NA of 0.14
+
+    variables_dict = {
+        'Wavelength (nm)': f'{wavelength / u.nm:.4g}',
+        'Laser Power (W)': f'{laser_power:.3g}',
+        'Pulse Rate (kHz)': f'{pulse_rate / u.kHz:.3g}',
+        'Pulse Duration (fs)': f'{pulse_duration / u.fs:.3g}',
+        'Numerical Aperture': f'{numerical_aperture:.3g}'
+    }
+
+    # Convert the dictionary into a DataFrame
+    variables_df = pd.DataFrame(variables_dict, index=['Value']).T
+
+    Beam = PulsedLaserBeam(wavelength, laser_power, pulse_rate, pulse_duration, beam_waist=15 * u.um)  # Change the beam waist to be 15µm
+
+else:
+    wavelength = st.number_input("Wavelength λ (nm)", min_value=300, max_value=1064, value=800) * u.nm  # default wavelength of 800 nm
+    laser_power = st.number_input("Laser power (W)", min_value=0.001, max_value=20.0, value=1.0) * u.W  # default power of 1 W
+    pulse_rate = st.number_input("Pulse rate (MHz)", min_value=0.01, max_value=1000.0, value=0.3) * u.MHz  # default pulse rate of 0.3 MHz
+    pulse_duration = st.number_input("Pulse Duration (fs)", min_value=1, max_value=500, value=100) * u.fs  # default pulse duration of 100 fs
+    numerical_aperture = st.number_input("Numerical Aperture (NA)", min_value=0.01, max_value=2.0, value=0.14)  # default NA of 0.14
+
+    variables_dict = {
+        'Wavelength (nm)': f'{wavelength / u.nm:.4g}',
+        'Laser Power (W)': f'{laser_power:.3g}',
+        'Pulse Rate (Hz)': f'{pulse_rate:.3g}',
+        'Pulse Duration (s)': f'{pulse_duration:.3g}',
+        'Numerical Aperture': f'{numerical_aperture:.3g}'
+    }
+
+    # Convert the dictionary into a DataFrame
+    variables_df = pd.DataFrame(variables_dict, index=['Value']).T
+
+    with col1:
         # Display the DataFrame in Streamlit
         st.dataframe(variables_df)
 
-    elif laster_option == "PHAROS":
-        wavelength = 1030 * 1e-9 # default wavelength of 640 nm
-        laser_power = 7 # default power of 1 W
-        pulse_rate = 200 * 1e3 # default pulse rate  kHz
-        pulse_duration = 100 * 1e-15 # default pulse duration of 100 fs
-        numerical_aperture = 0.14 # default NA of 0.1
-        variables_dict = {
-            'Wavelength (nm)': f'{wavelength*1e9:.4g}',
-            'Laser Power (W)': f'{laser_power:.3g}',
-            'Pulse Rate (KHz)': f'{pulse_rate*1e-3:.3g}',
-            'Pulse Duration (fs)': f'{pulse_duration*1e13:.3g}',
-            'Numerical Aperture': f'{numerical_aperture:.3g}'
-        }
-        # Convert the dictionary into a DataFrame
-        variables_df = pd.DataFrame(variables_dict, index=['Value']).T
-
-        # Display the DataFrame in Streamlit
-        st.dataframe(variables_df)
-        Beam = PulsedLaserBeam(wavelength, laser_power, pulse_rate, pulse_duration, beam_waist=15e-6)# Change the beam waist to be 15µm
-    else:
-        wavelength = st.number_input("Wavelength λ (nm)", min_value=300, max_value=1064, value=800) * 1e-9 # default wavelength of 640 nm
-        laser_power = st.number_input("Laser power (W)", min_value=0.001, max_value=20.0, value=1.0) # default power of 1 W
-        pulse_rate = st.number_input("Pulse rate (MHz)", min_value=0.01, max_value=1000.0, value=0.3) * 1e6 # default pulse rate of 80 MHz
-        pulse_duration = st.number_input("Pulse Duration (fs)", min_value=1, max_value=500, value=100) * 1e-16 # default pulse duration of 100 fs
-        numerical_aperture = st.number_input("Numerical Aperture (NA)", min_value=0.01, max_value=2.0, value=0.14) # default NA of 0.1
-
-st.header("Heating Dynamics")
-col1, spacer1, col2, spacer2= st.columns([1,0.3,1, 0.3])
-
-with col1:
-    st.markdown("There are multiple phases involved in the heating dynamics from ultra fast PLAL.")
-    st.markdown("The Two Tempreature Model (TTM) is an analytical model that treats the electron gas and lattice as seperate systems to model how the overall tempreture of the system evolves over time.")
-
-    st.latex(r'''c_e \frac{\partial T_e}{\partial t} = \text{div} \left( \kappa_e \nabla T_e \right) - \alpha \left( T_e - T_i \right) + Q''')
-    st.latex(r'''c_i \frac{\partial T_i}{\partial t} = \text{div} \left( \kappa_i \nabla T_i \right) + \alpha \left( T_e - T_i \right)''')
-with col2:
-    st.image('img/heating-flow-chart.png', caption="Phases of a solid dilectric material heading from an ultrafast PLAL", use_column_width=True)
-
-
+    with col2:
+        st.markdown("add gaussian beam profile settings vis plots")
 
 st.subheader("Two Temperature Model (TTM)")
+# -----
+# Setting up the TTM model
+# -----
+
+
 
 st.subheader("Ionisation Regime")
 
 st.subheader("Ablation Volumes")
 
+st.divider()
 st.header("Particle Ejection")
 
 st.subheader("Particle Size Distribution")
