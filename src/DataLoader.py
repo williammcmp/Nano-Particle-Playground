dff3b868b895e5dfb59488d46efa26b476d13e3d
William <william.mcm.p@gmail.com>
2024-08-29 15:08:27 +1000

    added save and loading data interfaces

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -422,3 +422,87 @@ def remove_offset(data: pd.DataFrame, wavelength: int = 300):
     corrected[move_mask] = corrected[move_mask] - lower_offset
 
     return corrected
+
+def save_dataframe(df: pd.DataFrame, file_path: str, force_format: str = None):
+    """
+    Save a pandas DataFrame to a specified file format.
+    
+    Parameters:
+    - df (pd.DataFrame): The DataFrame to save.
+    - file_path (str): The path where the DataFrame should be saved.
+    - force_format (str, optional): Desired file format to force. Supported formats: 'parquet' (recommended), 'csv', 'excel', 'txt'.
+
+    Returns:
+    - bool: True if saving was successful, False otherwise.
+    """
+    try:
+        # Determine the file format to use
+        file_format = force_format or file_path.split('.')[-1].lower()
+        
+        if file_format == 'parquet': # the binary type --> fastest
+            df.to_parquet(file_path)
+        elif file_format == 'csv':
+            df.to_csv(file_path, index=False)
+        elif file_format == 'excel' or file_format == 'xlsx':
+            df.to_excel(file_path, index=False)
+        elif file_format == 'txt':
+            df.to_csv(file_path, sep='\t', index=False)
+        elif file_format == "json":
+            write_to_json(df, file_path)
+  
+        else:
+            # Incase someone wants to save to an unsupported formate
+            raise ValueError(f"Unsupported file format: {file_format}")
+
+        print(f"DataFrame successfully saved to {file_path}")
+        return True
+
+    except Exception as e:
+        print(f"Error saving DataFrame: {e}")
+        return False
+    
+def load_dataframe(file_path: str, enable_pyarrow = False):
+    """
+    Load a pandas DataFrame from a specified file format.
+    
+    Parameters:
+    - file_path (str): The path to the file to load.
+    - enable_pyarrow (bool): Eanbled an optimised method for loading parquet files
+
+    Returns:
+    - pd.DataFrame: The loaded DataFrame if successful, None otherwise.
+    """
+    try:
+        # gets the file extension
+        file_format = file_path.split('.')[-1].lower()
+
+        
+        if file_format == 'parquet': # the binary type --> fastest
+            if enable_pyarrow:
+                # This may cause loading problems for strange DataFrames
+                df = pd.read_parquet(file_path, engine='pyarrow') # Use the C++ optimised engine for loading 
+            else:
+                df = pd.read_parquet(file_path) # let pandas decide what engine to use
+        elif file_format == 'csv':
+            load_data_from_csv(file_path)
+        elif file_format in ['xls', 'xlsx']: # For the various types of Excel file types
+            load_data_from_excel(file_path)
+        elif file_format == 'txt':
+            load_data_from_txt(file_path)
+        elif file_format == "json":
+            load_from_json(file_path)
+        else:
+            # For the losers who want to load un-listed formats
+            raise ValueError(f"Unsupported file format: {file_format}")
+
+        # Check if the DataFrame is empty
+        if df.empty:
+            print(f"Warning: The DataFrame loaded from {file_path} is empty.")
+            return None
+
+        print(f"DataFrame successfully loaded from {file_path}")
+        return df
+
+    except Exception as e:
+        print(f"Error loading DataFrame: {e}")
+        return None
